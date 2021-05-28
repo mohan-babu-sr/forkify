00e98877db2685b1aac688001c0e5ade010e594b
MOHAN BABU S R <83107890+mohan-drive@users.noreply.github.com>
2021-05-28 08:22:01 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'abedb0c5-352a-4b9c-af8f-1244b216c7c6';
+export const MODAL_CLOSE_SEC = 2.5;
